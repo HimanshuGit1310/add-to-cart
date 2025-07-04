b91ed9d298e80fccbab3c499f70d12b7b46ccd87
HimanshuGit1310 <himanshujhariyazeal@gmail.com>
2025-07-04 15:06:42 +0530

    feature: create product

--- src/main/java/com/zeal/FirstProject/service/ProductService.java
+++ src/main/java/com/zeal/FirstProject/service/ProductService.java
@@ -3,6 +3,7 @@ package com.zeal.FirstProject.service;
 import com.zeal.FirstProject.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -10,10 +11,10 @@ import java.util.stream.Stream;
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>( Arrays.asList(
             new Product(101,"Iphone",80000),
             new Product(102,"Camera",60000),
-            new Product(103,"Helmet",10000));
+            new Product(103,"Helmet",10000)));
 
 
     public List<Product> getProducts(){
@@ -23,7 +24,11 @@ public class ProductService {
     public Product getProductById(int prodId){
         return products.stream()
                 .filter(p-> p.getProdId()==prodId)
-                .findFirst().get();
+                .findFirst().orElse(new Product(100,"No item found",0));
+    }
+
+    public void addProduct(Product prod){
+        products.add(prod);
     }
 
 }
